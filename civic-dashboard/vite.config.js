e8d343613f828dcefc84ccf10a6df11a528886b1
Srikshith Rao <srikshith5120@gmail.com>
2025-09-28 22:10:54 +0530

    Heloadfa

--- civic-dashboard/vite.config.js
+++ civic-dashboard/vite.config.js
@@ -6,4 +6,5 @@ export default defineConfig({
   server: {
     port: 5173,
   },
+  base : process.env.VITE_BASE_PATH || "/JanSetu_1",
 })
--- civic-mobile/src/constants.js
+++ civic-mobile/src/constants.js
@@ -1,2 +1,2 @@
 // Point this to your backend API base URL
-export const API_URL = 'http://10.54.44.168:4000'
+export const API_URL = 'http://192.168.137.12:4000'
